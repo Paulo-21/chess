284cbdc456b1a4a254e9a2a77363df6c5cbc5629
Paulo-21 <paul.cibier@gmail.com>
2023-01-24 00:41:58 +0100

    some error but i will fix it later

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,14 +5,15 @@ use axum::{
     response::{ /*Html,*/ Response},
     Router,
 };
-use futures::{ sink::SinkExt, stream::StreamExt };
+use futures::{ sink::SinkExt, stream::StreamExt, lock };
 use std::{ net::SocketAddr };
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use tokio::sync:: { RwLock, broadcast };
 //use serde::Deserialize;
 use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value };
+use serde_json::{ Result, Value };
+use std::ops::Bound::Included;
 //use serde::Serialize;
 #[allow(dead_code)]
 struct ChessGame {
@@ -22,8 +23,9 @@ struct ChessGame {
 }
 #[derive(Deserialize, Clone)]
 struct User {
-    name : String,
+    user_name : String,
 }
+#[derive(Clone)]
 struct Player {
     name : String,
     elo : i16,
@@ -34,27 +36,50 @@ struct ChessMessage {
     sender : String,
     message : String,
 }
+struct Couille {
+    c : Arc<RwLock<BTreeMap::<String, ChessGame>>>,
+    b : Arc<RwLock<BTreeMap::<i16, Vec<Player>>>>,
+}
 
 #[tokio::main]
 async fn main() {
     let room_vec = BTreeMap::<String, ChessGame>::new();
-    let room = Arc::new(RwLock::new(room_vec));
-    let queue = BTreeMap::<i16, Player>::new();
-    let lock_queue = RwLock::new(queue);
-    
+    let lock_room = RwLock::new(room_vec);
+    let room = Arc::new(lock_room);
+    let room2 = room.clone();
+    let queue = BTreeMap::<i16, Vec<Player>>::new();
+    let lock_queue = Arc::new(RwLock::new(queue));
+
+    let couille = Couille {
+        c  : room.clone(),
+        b : lock_queue.clone(),
+    };
+    let cou = Arc::new(couille);
     let app = Router::new()
     .fallback(fallback)
-    .route("/websocketChess/:room_name/", get(handler))
-    .with_state(room);
+    .route("/websocketChess/Game/:room_name/", get(handler_game)).with_state(room)
+    .route("/websocketChess/Play/", get(handler_matchmaking)).with_state(cou)
+    .route("/websocketChess/Friend/", get(handler_friend)).with_state(room2);
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 8082));
     
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-  
-    async fn handler(axum::extract::Path(id):axum::extract::Path<String> , Query(user_name): Query<User>,  ws: WebSocketUpgrade , State(state): State<Arc<RwLock<BTreeMap::<String, ChessGame>>>> ) -> Response {
+
+    async fn handler_game(axum::extract::Path(id):axum::extract::Path<String> , Query(user_name): Query<User>,  ws: WebSocketUpgrade , State(state): State<Arc<RwLock<BTreeMap::<String, ChessGame>>>> ) -> Response {
         ws.on_upgrade(|socket| handle_socket(socket, state, id, user_name))
+    }
+    async fn handler_matchmaking( Query(user_name): Query<User>,  ws: WebSocketUpgrade , State(state): State<Arc<Couille>> ) -> Response {
+        let player = Player {
+            name : user_name.user_name,
+            elo : 1000,
+        };
+        ws.on_upgrade(|socket| handle_socket_matchmaking(socket, state, player))
+    }
+    async fn handler_friend( Query(user_name): Query<User>,  ws: WebSocketUpgrade , State(state): State<Arc<RwLock<BTreeMap::<String, ChessGame>>>> ) -> Response {
+        ws.on_upgrade(|socket| handle_socket_friend(socket, state, user_name))
     }
     async fn handle_socket( socket: WebSocket, lock_room : Arc<RwLock<BTreeMap::<String, ChessGame>>>, room_name : String, user : User ) {
         println!("NEW ");
@@ -85,7 +110,7 @@ async fn main() {
         let mut recv_task = tokio::spawn(async move {
             while let Some(Ok(Message::Text(text))) = receiver.next().await {
                 println!("Message : {}", text);
-                let m = ChessMessage { room_name : room_name.clone(), sender : user1.name.clone(), message : String::from("Bonjour")};
+                let m = ChessMessage { room_name : room_name.clone(), sender : user1.user_name.clone(), message : String::from("Bonjour")};
                 if let Ok(m) = serde_json::to_string(&m) {
                     let _ = tx.send(m);
                 }
@@ -98,6 +123,46 @@ async fn main() {
             _ = (&mut send_task) => recv_task.abort(),
             _ = (&mut recv_task) => send_task.abort(),
         };
+    }
+
+    async fn handle_socket_matchmaking( socket: WebSocket, couille : Arc<Couille>, user : Player ) {
+        let user_elo = user.elo;
+        let r1 = couille.b.read().await;
+        if (*r1).contains_key(&user.elo) {
+
+            drop(r1);
+            let mut w1 = couille.b.write().await;
+            let v = (*w1).get_mut(&user.elo).unwrap();
+            v.push(user);
+            drop(w1);
+        }
+        else {
+            drop(r1);
+            let mut w1 = couille.b.write().await;
+            
+            let v = Vec::from([user]);
+            (*w1).insert(user_elo, v);
+            drop(w1);
+        }
+
+        let mut w2 = couille.b.read().await;
+        let mut selected:Vec<(i16, Player)> = Vec::new();
+        let mut key_selected = 0;
+        let mut delta = i16::MAX;
+        for (&key, &value) in (*w2).range((Included(&(user_elo-100)), Included(&(user_elo+100)))) {
+            for x in value {
+                selected.push((key.clone(), x.clone()));
+            }
+        let d:i16 = user_elo - key;
+        let temp_delta = d.abs();
+        if temp_delta < delta  {
+            delta = temp_delta;
+            key_selected = key;
+        }
+    }
+    }
+    async fn handle_socket_friend( socket: WebSocket, lock_room : Arc<RwLock<BTreeMap::<String, ChessGame>>>, user : User ) {
+        
     }
     pub async fn fallback( uri: axum::http::Uri) -> impl axum::response::IntoResponse {
         println!("FALLBACK");
